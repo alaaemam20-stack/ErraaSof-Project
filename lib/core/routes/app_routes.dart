a5e69559ca6febebcd639992ee2b30539cb08f69
Alaa Emam <alaaemamm20@gmail.com>
2026-09-25 13:59:20 -0700

    add ui of register_screen.dart

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,6 +1,7 @@
 class AppRoutes {
 static const String loginScreen="loginScreen";
-static const String registerScreen="loginScreen";
+static const String registerScreen="registerScreen";
+static const String homeScreen="homeScreen";
 
 
 }
